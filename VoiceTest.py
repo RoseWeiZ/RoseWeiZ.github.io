c43915a641b3b1c3962815a1f7091ceb62af4ba2
RoseWeiZ <139348891+RoseWeiZ@users.noreply.github.com>
2025-04-21 18:57:45 +0100

    See available voices

--- VoiceTest.py
+++ VoiceTest.py
@@ -2,8 +2,13 @@ import openai
 import pyttsx3
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-for voice in voices:
+'''for voice in voices:
    engine.setProperty('voice', voice.id)
 
    engine.say('The quick brown fox jumped over the lazy dog.')
-engine.runAndWait()
+engine.runAndWait()'''
+for voice in voices:
+    engine.setProperty('voice', voice.id)
+    print(f"Now using: {voice.name} - {voice.id}")
+    engine.say('The quick brown fox jumped over the lazy dog.')
+    engine.runAndWait()
